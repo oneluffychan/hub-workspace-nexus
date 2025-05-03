de1fa988eedb13c09ded51a27171c51a1a6273e9
Varshith V Hegde <varshithvh@gmail.com>
2025-05-03 10:55:06 +0530

    Refactor: Remove WorkspaceProvider from main rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-
 import { createRoot } from 'react-dom/client'
-import { WorkspaceProvider } from './contexts/WorkspaceContext'
 import { AuthProvider } from './contexts/AuthContext'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
-    <WorkspaceProvider>
-      <App />
-    </WorkspaceProvider>
+    <App />
   </AuthProvider>
 );
